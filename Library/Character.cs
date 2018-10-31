c56b8a43ac003d460a92ce648bff4b0f19a80cd1
Joseph Hicks <joseph.hicks@americanenterprise.com>
2018-10-31 14:49:27 -0500

    test commit

---
+++ Library/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Character
+    {
+        // Physical
+        public Attribute Strength { get; set; }
+        public Attribute Dexterity { get; set; }
+        public Attribute Stamina { get; set; }
+
+        public List<Attribute> PhysicalAttributes { get; } = new List<Attribute>();
+
+        // Social
+        public Attribute Charisma { get; set; }
+        public Attribute Manipulateion { get; set; }
+        public Attribute Appearance { get; set; }
+        public List<Attribute> SocialAttributes { get; } = new List<Attribute>();
+
+        // Mental
+        public Attribute Intelligence { get; set; }
+        public Attribute Perception { get; set; }
+        public Attribute Wits { get; set; }
+        public List<Attribute> MentalAttributes { get; } = new List<Attribute>();
+    }
+}
---
+++ Library/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+    }
+}
